5f817993288e7a55656bd6fd8ed4ae8c6eab3161
Eros Mendonça <erosmendonca@outlook.com>
2024-09-28 13:24:31 +0100

    feat: simple api response

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,14 @@
 import express, { Request, Response } from "express";
+import { readFile } from "fs/promises";
+import { resolve } from "path";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+app.get("/", async (req: Request, res: Response) => {
+  const filePath = resolve(__dirname, "../db/data.json");
+  const json = await readFile(filePath, "utf-8");
+  res.json(JSON.parse(json));
 });
 
 app.listen(port, () => {
