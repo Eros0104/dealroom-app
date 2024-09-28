6817ad8f69da31b1006b8eb8bd511d45b2a05d45
Eros Mendonça <erosmendonca@outlook.com>
2024-09-28 15:44:08 +0100

    feat: adding cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response } from "express";
 import { readFile } from "fs/promises";
 import { resolve } from "path";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.get("/api/companies", async (req: Request, res: Response) => {
   const filePath = resolve(__dirname, "../db/data.json");
