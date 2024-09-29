a2cdcf3fb4b80ba019f18a70c73db6e3ba35de98
Eros Mendonça <erosmendonca@outlook.com>
2024-09-29 21:31:13 +0100

    chore: cors in api

--- backend/netlify/functions/api.ts
+++ backend/netlify/functions/api.ts
@@ -1,11 +1,9 @@
 import express from "express";
 import serverless from "serverless-http";
 import router from "../../src/routes";
-import cors from "cors";
 
 const api = express();
 
 api.use("/api/", router);
-api.use(cors());
 
 export const handler = serverless(api);
