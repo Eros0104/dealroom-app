109e6d08b6ab60fc9bb48f4156161068f6405c4f
Eros Mendonça <erosmendonca@outlook.com>
2024-09-29 19:27:05 +0100

    fix: fetching json from same domain

--- backend/src/controllers/company.controller.ts
+++ backend/src/controllers/company.controller.ts
@@ -1,12 +1,10 @@
 import { Request, Response } from "express";
 import { readFile } from "fs/promises";
-import { resolve } from "path";
 import jsonParser from "../utils/json-parser";
 
 const companyController = {
   getCompanies: async (req: Request, res: Response) => {
-    const filePath = resolve(__dirname, "../../db/data.json");
-    const json = await readFile(filePath, "utf-8");
+    const json = await readFile("db/data.json", "utf-8");
     const result = jsonParser.parse(json);
     res.json(result);
   },
