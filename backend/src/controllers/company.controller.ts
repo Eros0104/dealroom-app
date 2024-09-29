f3c20b3d104453be12d0da17132209acdf8fd7c3
Eros Mendonça <erosmendonca@outlook.com>
2024-09-29 19:35:25 +0100

    fix: attempt to fix json import

--- backend/src/controllers/company.controller.ts
+++ backend/src/controllers/company.controller.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from "express";
-import { readFile } from "fs/promises";
 import jsonParser from "../utils/json-parser";
+import * as data from "../../db/data.json";
 
 const companyController = {
   getCompanies: async (req: Request, res: Response) => {
-    const json = await readFile("db/data.json", "utf-8");
+    const json = JSON.stringify(data);
     const result = jsonParser.parse(json);
     res.json(result);
   },
